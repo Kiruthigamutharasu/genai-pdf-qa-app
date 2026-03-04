09e2cb1bad75ac3a0afad48780ff72511780be90
Kiruthigamutharasu <kiruthigamutharasu15@gmail.com>
2026-03-04 20:32:45 +0530

    Fix RetrievalQA import for LangChain

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.chains import RetrievalQA
+from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain_community.llms import HuggingFacePipeline
 
 from transformers import pipeline
